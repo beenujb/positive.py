b5cf2e4252029556070ab7ab4fb275c42852cd35
beenujb <41043473+beenujb@users.noreply.github.com>
2018-12-18 16:04:03 +0530

    Update numeric.py

--- numeric.py
+++ numeric.py
@@ -1,6 +1,6 @@
 count = 0
-number = int(input("Enter a number "))
-while (number > 0):
-  number = number//10
+num = int(input("Enter a number "))
+while (num > 0):
+  num = num//10
   count = count + 1
 print ("Total number of digits : ",count)
