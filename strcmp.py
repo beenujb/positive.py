19120e2475dbddafa12837082a976a87da289ecd
beenujb <41043473+beenujb@users.noreply.github.com>
2018-12-18 08:52:02 +0530

    Update strcmp.py

--- strcmp.py
+++ strcmp.py
@@ -1,16 +1,16 @@
-string1=raw_input("Enter first string:")
-string2=raw_input("Enter second string:")
+str1=raw_input("Enter first string:")
+str2=raw_input("Enter second string:")
 count1=0
 count2=0
-for i in string1:
+for i in str1:
       count1=count1+1
-for j in string2:
+for j in str2:
       count2=count2+1
 if(count1<count2):
       print("Larger string is:")
-      print(string2)
+      print(str2)
 elif(count1==count2):
       print("Both strings are equal.")
 else:
       print("Larger string is:")
-      print(string1)
+      print(str1)
